48af043ad6627dbe92c85c4ae9ae5ef1ca2ef684
Peter Sobot <psobot@gmail.com>
2012-11-06 15:43:32 -0500

    Updated brain to take into account many more factors and fixed duplication errors.

--- forever/brain.py
+++ forever/brain.py
@@ -2,10 +2,13 @@ import sys
 import tsp
 import shlex
 import config
+import difflib
 import logging
 import traceback
 import soundcloud
 from timer import Timer
+from random import shuffle
+from operator import itemgetter
 
 log = logging.getLogger(__name__)
 test = 'test' in sys.argv
@@ -13,6 +16,8 @@ client = soundcloud.Client(client_id=config.SOUNDCLOUD_CLIENT_KEY)
 
 TAG_WEIGHT = config.tag_weight
 BPM_WEIGHT = config.bpm_weight
+LEN_WEIGHT = config.len_weight
+GENRE_WEIGHT = config.genre_weight
 SPREAD_WEIGHT = config.spread_weight
 NO_BPM_DIFF = config.no_bpm_diff
 
@@ -32,11 +37,15 @@ def update_weights():
     """
     global TAG_WEIGHT
     global BPM_WEIGHT
+    global LEN_WEIGHT
+    global GENRE_WEIGHT
     global SPREAD_WEIGHT
     global NO_BPM_DIFF
 
     TAG_WEIGHT = config.tag_weight
     BPM_WEIGHT = config.bpm_weight
+    LEN_WEIGHT = config.len_weight
+    GENRE_WEIGHT = config.genre_weight
     SPREAD_WEIGHT = config.spread_weight
     NO_BPM_DIFF = config.no_bpm_diff
 
@@ -46,7 +55,10 @@ class DeduplicatedTrack(object):
         self.o = o
 
     def __eq__(self, other):
-        return self.o.title == other.title
+        return hash(self) == hash(other)
+
+    def __hash__(self):
+        return hash(self.o.title.lower())
 
 
 def tag_diff(a, b):
@@ -57,19 +69,29 @@ def tag_diff(a, b):
 
 
 def bpm_diff(a, b):
-    try:
-        #   BPM might not be defined
-        return BPM_WEIGHT * abs(a.bpm - b.bpm)
-    except TypeError:
-        return BPM_WEIGHT * NO_BPM_DIFF
+    if a.bpm < 200 and b.bpm < 200:
+        try:
+            return BPM_WEIGHT * abs(a.bpm - b.bpm)
+        except TypeError:
+            pass
+    return BPM_WEIGHT * NO_BPM_DIFF
+
+
+def len_diff(a, b):
+    return abs(a.duration - b.duration) / 100.0
 
 
 def spread_diff(a, b):
     return SPREAD_WEIGHT * int(a.user['username'] == b.user['username'])
 
 
+def genre_diff(a, b):
+    r = difflib.SequenceMatcher(a=a.genre.lower(), b=b.genre.lower()).ratio()
+    return GENRE_WEIGHT * (1.0 - r)
+
+
 def distance(a, b):
-    return bpm_diff(a, b) + tag_diff(a, b) + spread_diff(a, b)
+    return bpm_diff(a, b) + len_diff(a, b) + tag_diff(a, b) + genre_diff(a, b) + spread_diff(a, b)
 
 
 def chunks(l, n):
@@ -113,8 +135,7 @@ def add_tracks():
             update_weights()
             log.info("Grabbing fresh tracklist from SoundCloud...")
             with Timer() as t:
-                tracks = cull(client.get('/tracks', order='hotness',
-                                        limit=l, offset=0))
+                tracks = cull(client.get('/tracks', order='hotness', limit=l, offset=0))
             log.info("Got %d tracks in %2.2fms.", len(tracks), t.ms)
             log.info("Solving TSP on %d tracks...", len(tracks))
             with Timer() as t:
@@ -148,5 +169,23 @@ def pprint(track):
 
 if __name__ == "__main__":
     print "Testing the BRAIN..."
-    for track in add_tracks():
-        pprint(track)
+    if "weight" in sys.argv:
+        tracks = cull(client.get('/tracks', order='hotness', limit=1000, offset=0))
+        shuffle(tracks)
+        print "Comparing against", tracks[0].title
+        print "\t", tracks[0].tag_list
+        print "\t", tracks[0].genre
+        print "DIST\tBPM\tLEN\tTAG\tGEN\tSPR\tTITLE"
+        for track, dist, bpm, l, tag, genre, spread in sorted([(
+            track,
+            distance(tracks[0], track),
+            bpm_diff(tracks[0], track),
+            len_diff(tracks[0], track),
+            tag_diff(tracks[0], track),
+            genre_diff(tracks[0], track),
+            spread_diff(tracks[0], track),
+        ) for track in tracks], key=itemgetter(1)):
+            print round(dist, 1), "\t", round(bpm, 1), "\t", round(l, 1), "\t", round(tag, 1), "\t", round(genre, 1), "\t", round(spread, 1), "\t", track.title, "\t", track.genre
+    else:
+        for track in add_tracks():
+            pprint(track)
