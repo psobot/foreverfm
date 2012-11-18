0902348f8e962fab4764462a51283abf81b678e6
Peter Sobot <psobot@gmail.com>
2012-11-18 02:44:47 -0500

    More relay fixes.

--- forever/relay.py
+++ forever/relay.py
@@ -22,7 +22,6 @@ import tornado.web
 import tornado.ioloop
 import tornado.template
 from daemon import Daemon
-from functools import partial
 
 started_at_timestamp = time.time()
 r = urllib2.urlopen(config.primary_url)
@@ -32,34 +31,32 @@ class StreamHandler(tornado.web.RequestHandler):
 
     @classmethod
     def stream_frames(cls):
+        global r
         l = tornado.ioloop.IOLoop.instance()
         while True:
             try:
-                l.add_callback(partial(cls.broadcast, r.read(256)))
-            except:
+                cls.__packet = r.read(128)
+                for i, listener in enumerate(cls.listeners):
+                    if listener.request.connection.stream.closed():
+                        try:
+                            listener.finish()
+                        except:
+                            pass
+                    else:
+                        listener.write(packet)
+                        listener.flush()
+            except urllib2.URLError:
                 try:
                     r = urllib2.urlopen(config.primary_url)
                 except:
                     log.error("Could not reopen stream!\n%s", traceback.format_exc())
-
-    @classmethod
-    def broadcast(cls, packet):
-        for i, listener in enumerate(cls.listeners):
-            if listener.request.connection.stream.closed():
-                try:
-                    listener.finish()
-                except:
-                    pass
-            else:
-                listener.write(packet)
-                listener.flush()
-
 
     @tornado.web.asynchronous
     def get(self):
         ip = self.request.headers.get('X-Real-Ip', self.request.remote_ip)
         log.info("Added new listener at %s", ip)
         self.set_header("Content-Type", "audio/mpeg")
+        self.write(self.__packet)
         self.listeners.append(self)
 
     def on_finish(self):
