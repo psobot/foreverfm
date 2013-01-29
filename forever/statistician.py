13ce3a3818c80e1bd5cedfc2d7ebaebdb91c2b42
Peter Sobot <psobot@gmail.com>
2013-01-28 21:54:52 -0800

    Added proper cube logging for listener counts.

--- forever/statistician.py
+++ forever/statistician.py
@@ -1,11 +1,14 @@
 import time
 import config
+from cube import emit
 
 
 def generate(get_listeners, get_stats, **queues):
     while True:
         time.sleep(config.monitor_update_time)
+        listeners = get_listeners()
+        emit("listeners", {"count": len(listeners)})
         yield {"listeners": [dict(dict(g.request.headers).items() + [("remote_ip", g.request.remote_ip)])
-                            for g in get_listeners()],
+                            for g in listeners],
                "queues": dict([(n, q.buffered) for n, q in queues.iteritems()]),
                "info": get_stats()}
